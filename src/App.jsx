21cb806d4bebc5bed8f424ec2dbd4b27bda5f850
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-08-12 23:04:27 +0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import s from "./App.module.css";
+import {
+  selectContacts,
+  selectLoading,
+  selectError,
+} from "./redux/contactsSlice";
+import { changeFilter, selectNameFilter } from "./redux/filtersSlice";
 
 const App = () => {
+const dispatch = useDispatch();
+const contacts = useSelector(selectContacts);
+const loading = useSelector(selectLoading);
+const error = useSelector(selectError);
+const filter = useSelector(selectNameFilter); 
+  
+ useEffect(() => {
+   dispatch(fetchContacts());
+ }, [dispatch]);
+
+  
+
+  const handleAddContact = (newContact) => {
+    dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = (id) => {
+    dispatch(deleteContact(id));
+  };
+
+  const handleFilterChange = (value) => {
+    dispatch(changeFilter(value));
+  };
+
   return (
     <div className={s.container}>
       <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <SearchBox />
-      <ContactList />
+      <ContactForm onAddContact={handleAddContact} />
+      <SearchBox filter={filter} onFilter={handleFilterChange} />
+      {loading && <p>Loading contacts...</p>}
+      {error && <p>Error: {error}</p>}
+      <ContactList onDelete={handleDeleteContact} />
     </div>
   );
 };
