951cd8170641d4c581542a3a9afe64195c9e25b0
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-08-14 23:48:50 +0300

    css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import { deleteContact } from "../../redux/contactsOps";
 import { FaPhone, FaUser } from "react-icons/fa";
 import s from "/src/components/Contact/Contact.module.css";
 
-const Contact = ({ contact}) => {
+const Contact = ({ contact }) => {
   const dispatch = useDispatch();
 
   const handleDelete = () => {
@@ -11,11 +11,18 @@ const Contact = ({ contact}) => {
   };
 
   return (
-    <li className={s.wrapper}>
-      <span className={s.contactNumber}>
-        {contact.name}: {contact.number}
-      </span>
-      <button className={s.formBtn} onClick={handleDelete}>
+    <li className={s.contactItem}>
+      <div className={s.contactDetails}>
+        <span className={s.contactSpan}>
+          <FaUser />
+          <p>{contact.name}</p>
+        </span>
+        <span className={s.contactSpan}>
+          <FaPhone />
+          <p>{contact.number}</p>
+        </span>
+      </div>
+      <button onClick={handleDelete} className={s.deleteButton}>
         Delete
       </button>
     </li>
