d0814af1f04d214b755f18b53d4d4fc00f24d5b6
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-08-16 00:24:45 +0300

    action.payload

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -14,7 +14,7 @@ const contactsSlice = createSlice({
     builder
       .addCase(fetchContacts.pending, (state) => {
         state.loading = true;
-        
+        state.error = null;
       })
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.loading = false;
@@ -43,7 +43,7 @@ const contactsSlice = createSlice({
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.loading = false;
         state.items = state.items.filter(
-          (contact) => contact.id !== action.payload.id // Виправлено
+          (contact) => contact.id !== action.payload 
         );
       })
       .addCase(deleteContact.rejected, (state, action) => {
